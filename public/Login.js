391a563914f63965e464f4e5905eabcfa7c7822f
kaushal_03 <ksiriguppa@gmail.com>
2023-05-27 01:25:29 +0530

    Added Chart for each coins

--- public/Login.js
+++ public/Login.js
@@ -24,7 +24,7 @@ function Login() {
           <Stack align={'center'}>
             <Heading fontSize={'4xl'}>Sign in to your account</Heading>
             <Text fontSize={'lg'} color={'gray.600'}>
-              to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+              to enjoy all of our cool <Link color={'blue.400'}>featurese</Link> ✌️
             </Text>
           </Stack>
           <Box
